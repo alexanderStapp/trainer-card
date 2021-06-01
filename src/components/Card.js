4c447991545ae69ebb49354731de1d830de74c3d
alex <alexanderstapp@gmail.com>
2021-06-01 16:47:36 -0600

    working on animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,16 @@ import {useSpring, animated} from 'react-spring'
 function Card(props) {
     const [userInfo, setUserInfo] = useState(null)
     const [editView, setEditView] = useState(false)
+    const [flipped, setFlip] = useState(false)
     const [trades, setTrades] = useState([])
     const {handleEdit} = useContext(CardContext)
     const {user} = useContext(UserContext)
     const {username} = props.match.params
+    const {transform, opacity} = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+    })
 
     useEffect(() => {
         axios.get(`/api/card/${username}`)
@@ -35,33 +41,38 @@ function Card(props) {
     }, [username])
 
     return (
-        <div className='trainer-card'>
-            {userInfo && (<>
-                <img className='card-pic' src={userInfo.profile_pic} alt={userInfo.username} />
-                <span className='card-name'>
-                    <h3>NAME:</h3>
-                    <h3>{userInfo.username}</h3>
-                </span>
-                <span className='card-threeds'>
-                    <h3>3DS ID:</h3>
-                    <h3>{userInfo.threeds}</h3>
-                </span>
-                <span className='card-switch'>
-                    <h3>SWITCH ID:</h3>
-                    <h3>{userInfo.switch}</h3>
-                </span>
-                <span className='card-home'>
-                    <h3>HOME ID:</h3>
-                    <h3>{userInfo.home.toUpperCase()}</h3>
-                </span>
-                {trades.map(trade => {
-                    return (
-                        <div key={trade.trade_id} className='trade-item'>
-                            <h3>looking for {trade.name1} <img src={trade.sprite1} alt={trade.name1} /> willing to trade {trade.name2} <img src={trade.sprite2} alt={trade.name2} /></h3>
-                        </div>
-                    )
-                })}
-            </>)}
+        <div onClick={() => setFlip(flipped => !flipped)} className='trainer-card'>
+            <animated.div className="trainer-card back" style={{opacity: opacity.to(o => 1 - o), transform}} >
+                {userInfo && (<>
+                    <img className='card-pic' src={userInfo.profile_pic} alt={userInfo.username} />
+                    <span className='card-name'>
+                        <h3>NAME:</h3>
+                        <h3>{userInfo.username}</h3>
+                    </span>
+                    <span className='card-threeds'>
+                        <h3>3DS ID:</h3>
+                        <h3>{userInfo.threeds}</h3>
+                    </span>
+                    <span className='card-switch'>
+                        <h3>SWITCH ID:</h3>
+                        <h3>{userInfo.switch}</h3>
+                    </span>
+                    <span className='card-home'>
+                        <h3>HOME ID:</h3>
+                        <h3>{userInfo.home.toUpperCase()}</h3>
+                    </span>
+                    {trades.map(trade => {
+                        return (
+                            <div key={trade.trade_id} className='trade-item'>
+                                <h3>looking for {trade.name1} <img src={trade.sprite1} alt={trade.name1} /> willing to trade {trade.name2} <img src={trade.sprite2} alt={trade.name2} /></h3>
+                            </div>
+                        )
+                    })}
+                </>)}
+            </animated.div>
+            <animated.div className="trainer-card front" style={{opacity, transform: transform.to(t => `${t} rotateX(180deg)`)}}>
+                <p>hello</p>
+            </animated.div>
             <button className='undo-qr'><RiQrCodeFill /></button>
             {editView && <button className='edit-save' onClick={handleEdit}><RiPencilFill /></button>}
         </div>
