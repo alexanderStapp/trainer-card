7d9fa0c9ae9d32052a9d2a8f1a52ab3dd7af2875
alex <alexanderstapp@gmail.com>
2021-06-09 12:26:28 -0600

    nodemailer controller creation

---
+++ server/controllers/mailController.js
@@ -0,0 +1,24 @@
+import {createTransport} from 'nodemailer';
+
+const transporter = createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'youremail@gmail.com',
+        pass: 'yourpassword'
+    }
+});
+
+const mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'alexanderstapp@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
