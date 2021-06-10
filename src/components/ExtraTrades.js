b052aac9ba985738afb5446dd313d70202ab0c48
alex <alexanderstapp@gmail.com>
2021-06-10 00:01:11 -0600

    fixed any pokemon img in extra trades

--- src/components/ExtraTrades.js
+++ src/components/ExtraTrades.js
@@ -10,20 +10,41 @@ function ExtraTrades(props) {
             {tradesExtra.map(trade => {
                 return (
                     <div key={trade.trade_id} className='trade-item-extra'>
-                        <div className='icon-wrapper'>
-                            <img className='looking-icon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/${trade.poke_id1}.png`} alt={trade.name1}/>
-                        </div>
+                            {trade.name1 === 'any pokemon' ?
+                                <div className='icon-wrapper'>
+                                    <img className='willing-icon' src='https://cdn2.bulbagarden.net/upload/8/8e/Spr_3r_000.png' alt={trade.name2}/>
+                                </div>
+                                :
+                                <div className='icon-wrapper'>
+                                    <img className='willing-icon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/${trade.poke_id2}.png`} alt={trade.name2}/>
+                                </div>
+                            }
                         <span className='trade-message'>
                             <h3 className='looking-for'>looking for {trade.name1}</h3>
                             <h3 className='willing-to'>willing to trade {trade.name2}</h3>
                         </span>
-                            {props.editView && <button className='delete-trade' onClick={(e) => {
+                            {/* {props.editView && <button className='delete-trade' onClick={(e) => {
                                 handleDelete(trade.trade_id)
                                 e.stopPropagation()
-                            }}><RiCloseCircleFill /></button>}
-                            <div className='icon-wrapper'>
-                                <img className='willing-icon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/${trade.poke_id2}.png`} alt={trade.name2}/>
-                            </div>
+                            }}><RiCloseCircleFill /></button>} */}
+                            {trade.name2 === 'any pokemon' ?
+                                <div className='icon-wrapper'>
+                                    <img className='willing-icon' src='https://cdn2.bulbagarden.net/upload/8/8e/Spr_3r_000.png' alt={trade.name2}/>
+                                    {props.editView && <button className='delete-trade' onClick={(e) => {
+                                        handleDelete(trade.trade_id)
+                                        e.stopPropagation()
+                                    }}><RiCloseCircleFill /></button>}
+                                </div>
+                                
+                                :
+                                <div className='icon-wrapper'>
+                                    <img className='willing-icon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/${trade.poke_id2}.png`} alt={trade.name2}/>
+                                    {props.editView && <button className='delete-trade' onClick={(e) => {
+                                        handleDelete(trade.trade_id)
+                                        e.stopPropagation()
+                                    }}><RiCloseCircleFill /></button>}
+                                </div>
+                            }
                     </div>
                 )
             })}
