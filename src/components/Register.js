1eccedbe1bfe347f8b6794a49f5a28ee81f96634
alex <alexanderstapp@gmail.com>
2021-06-10 09:33:30 -0600

    register styling

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,7 +8,7 @@ function Register() {
         <form className='register'>
             <h1>Sign Up</h1>
             <input className='input' placeholder='username' value={username} onChange={e => setUsername(e.target.value)} />
-            <input className='email' placeholder='email' value={email} onChange={e => setEmail(e.target.value)} />
+            <input className='input' placeholder='email' value={email} onChange={e => setEmail(e.target.value)} />
             <input type='password' placeholder='password' className='input' value={password} onChange={e => setPassword(e.target.value)} />
             <div className='auth-buttons'>
                 <button className='style-button' onClick={() => handleRegister(username, email, password)}>Register</button>
