8a04c7003f66bf56192fffe9c1466a835b3522eb
alex <alexanderstapp@gmail.com>
2021-06-13 01:18:08 -0600

    added toastify

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import './SCSS/App.css';
 import routes from './routes'
 import Header from './components/Header'
 import Audio from './components/Audio'
+import {ToastContainer} from 'react-toastify'
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+      />
       <div>
         <Header />
         <Audio />
--- src/context/BuddyContext.js
+++ src/context/BuddyContext.js
@@ -1,15 +1,22 @@
 import axios from 'axios'
 import {createContext, useState} from 'react'
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 export const BuddyContext = createContext()
 
+toast.configure()
+
 export const BuddyProvider = (props) => {
     const [buddies, setBuddies] = useState([])
+    const addSuccess = () => toast.success('buddy added!')
 
     const addBuddy = (buddy) => {
         axios.post(`/api/dash/${buddy}`)
+        .then(addSuccess()).catch(err => console.log(err))
     }
+
     const removeBuddy = (buddy) => {
         axios.delete(`/api/dash/${buddy}`)
             .then(res => {
